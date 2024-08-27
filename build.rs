06681217ee19d9659b210726766051bd1cca87aa
TechPersonYT <39715756+TechPersonYT@users.noreply.github.com>
2024-08-27 14:28:56 -0500

    Fix "procedure entry point" error on Windows

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use anyhow::Result;
 use vergen::EmitBuilder;
 
 fn main() -> Result<()> {
+    embed_resource::compile("manifest.rc", embed_resource::NONE);
     EmitBuilder::builder()
         .build_date()
         .git_describe(true, false, None)
